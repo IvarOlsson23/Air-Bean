46f62d15c2f0b572db2dfaa8b29e622f4ee0742c
Ivar Olsson <ivar.olsson2323@gmail.com>
2021-06-23 10:39:18 +0200

    Profil-fetch

--- src/views/Profil.js
+++ src/views/Profil.js
@@ -1,27 +1,25 @@
 import '../views/css/Profil.css'
+import axios from 'axios'
 import {connect} from 'react-redux'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, } from 'react'
 import {HiOutlineMinus} from 'react-icons/hi'
 import {GiNinjaHead} from 'react-icons/gi'
 // import { useState } from 'react'
 const Profil = ({LoginCreds}) => {
 
-    console.log(LoginCreds,'Från LoginCreds')
-    useEffect(() => {
-            fetchItems()
-        },[]);
 
     const [items, setItems] = useState([]);
+    const username = LoginCreds.name
+   useEffect(() => {
 
-    //Dynamiskt fetcha med det inloggade USER ID:t.
-    const fetchItems = async () => {
-        const data = await fetch('http://localhost:5000/api/order/zhmEi70WSxPtJYMFxgKuK')
-
-        let items = await data.json();
-        console.log(items,'från Profil');
-        setItems(items)
-    };
-
+    async function fetchProfile() {
+      const response = await axios.get(
+        `http://localhost:3001/api/order/${username}`
+      );
+      setItems(response.data);
+  };
+  fetchProfile()
+},[username] );
 
 
     return (
